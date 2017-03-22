592fa13b7c5bba2c50a6cbdb7c0f2ddd8a6e4eb0
xinchrome <xinchrome@gmail.com>
2017-03-22 14:35:17 +0800

    pretrain cnn before gan

--- gan/gan.py
+++ gan/gan.py
@@ -25,29 +25,43 @@ class HawkesGAN(object):
 		nb_type = len(full_sequences[0][0])
 		nb_feature = len(full_sequences[0][0][0])
 
+		# pretrain generator
+		if self.gen.params is None:
+			raise Exception('generator not pretrained')
+
+		# compile keras models
 		self.gen.create_trainable_model(train_sequences,pred_length)
-		self.gen.model.compile(optimizer='adam', loss='msle', metrics=['accuracy'])
 		self.dis.create_trainable_model(nb_event + pred_length,nb_type,nb_feature)
 		self.dis.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
 		self.dis.model.trainable = False
 		for l in self.dis.model.layers: l.trainable = False
-
 		x = Input(shape=(1,), dtype='int32')
 		y = self.gen.model(x)
 		z = self.dis.model(y)
-
 		gan_model = Model(input=[x], output=[z])
 		gan_model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-		# self.gen.model.summary()
-		# self.dis.model.summary()
-		# gan_model.summary()
+		# pretrain discrimnator
+		X = np.arange(nb_sequence)
+		Y = self.gen.model.predict(X,batch_size=1)
+		print {
+			'np.mean(Y[:,15:])':np.mean(Y[:,15:]),
+			'np.mean(Y[:,0:15])':np.mean(Y[:,0:15]),
+			'np.mean(np.array(full_sequences)[:,15:])':np.mean(np.array(full_sequences)[:,15:]),
+			'np.mean(np.array(full_sequences)[:,0:15])':np.mean(np.array(full_sequences)[:,0:15]),
+			'np.mean(np.array(train_sequences)[:,0:15])':np.mean(np.array(train_sequences)[:,0:15]),
+		}
+		Y = np.array([Y[i/2] if i % 2 == 0 else full_sequences[i/2] for i in range(2 * nb_sequence)])
+		Z = np.array([[0.,1.] if i % 2 == 0 else [1.,0.] for i in range(2 * nb_sequence)])
+		self.dis.model.fit(Y,Z,batch_size=1,epochs=1,verbose=1,validation_split=0.2)
 
-		self.gen.model.fit(
-			[np.arange(nb_sequence)[0:1]],[np.array(full_sequences)[0:1]],
-			nb_epoch=500, batch_size=1,
-			verbose=1,validation_split=0.0)
+		self.gen.model.summary()
+		self.dis.model.summary()
+		gan_model.summary()
+
+
+		# full train gan model
 
 		# self.dis.model.fit(
 		# 	[np.array(full_sequences)],[np.array([[0.,1.] for i in xrange(nb_sequence)])],
@@ -60,16 +74,14 @@ class HawkesGAN(object):
 		# 	verbose=1,validation_split=0.2)
 
 
-	def pre_train_hawkes(self,paper_data='../data/paper3.txt'):
-		loaded = self.gen.load(paper_data)
-		self.gen.pre_train(*loaded,max_outer_iter=10)
+	# def pre_train_hawkes(self,paper_data='../data/paper3.txt'):
 
-	def pre_train_cnn(self,paper_data='../data/paper3.txt'):
-		sequences,labels,features,publish_years,pids,superparams = self.dis.load(paper_data)
-		self.dis.model.fit(
-			[numpy.array(sequences)], [numpy.array(labels)],
-			nb_epoch=500, batch_size=1,
-			verbose=1,validation_split=0.2)
+	# def pre_train_cnn(self,paper_data='../data/paper3.txt'):
+	# 	sequences,labels,features,publish_years,pids,superparams = self.dis.load(paper_data)
+	# 	self.dis.model.fit(
+	# 		[numpy.array(sequences)], [numpy.array(labels)],
+	# 		nb_epoch=500, batch_size=1,
+	# 		verbose=1,validation_split=0.2)
 
 	def load(self,f,pred_length=10,train_length=15):
 		# features[paper][feature], sequences[paper][day][feature]
@@ -113,19 +125,24 @@ class HawkesGAN(object):
 			features.append(feature)
 			publish_years.append(publish_year)
 
-		for i in range(I):
 			sequence = []
 			for year in range(train_length) :
 				self_count = len([x for x in self_seq if year <= x and x < year + 1])
 				nonself_count = len([x for x in nonself_seq if year <= x and x < year + 1])
 				sequence.append([[self_count],[nonself_count]])
-			for year in range(train_length,int(pred_length + train_length)) :
-				self_count = len([x for x in self_seq if year <= x and x < year + 1]) * 1.5
-				nonself_count = len([x for x in nonself_seq if year <= x and x < year + 1]) * 1.5
-				sequence.append([[self_count],[nonself_count]])
+			# for year in range(train_length,int(pred_length + train_length)) :
+			# 	self_count = len([x for x in self_seq if year <= x and x < year + 1]) * 1.5
+			# 	nonself_count = len([x for x in nonself_seq if year <= x and x < year + 1]) * 1.5
+				# sequence.append([[self_count],[nonself_count]])
 			train_sequences.append(sequence)
 
 		superparams = {}
+		# print {
+		# 	# 'np.mean(np.array(full_sequences)[:,15:])':np.mean(np.array(full_sequences)[:,15:]),
+		# 	'np.mean(np.array(full_sequences)[:,0:15])':np.mean(np.array(full_sequences)[:,0:15]),
+		# 	'np.mean(np.array(train_sequences)[:,0:15])':np.mean(np.array(train_sequences)[:,0:15]),
+		# }
+		# exit()
 		return full_sequences,train_sequences,features,publish_years,pids,superparams
 
 
@@ -135,6 +152,13 @@ if __name__ == '__main__':
 		old_stdout = sys.stdout
 		sys.stdout = f
 		gan = HawkesGAN()
+		try:
+			gan.gen.params = json.load(open('../data/paper.pretrain.params.json'))
+		except:
+			loaded = gan.gen.load('../data/paper3.txt')
+			gan.gen.pre_train(*loaded,max_outer_iter=1)
+			with open('../data/paper.pretrain.params.json','w') as fw:
+				json.dump(gan.gen.params,fw)
 		loaded = gan.load('../data/paper3.txt')
 		gan.full_train(*loaded)
 		sys.stdout = old_stdout
--- gan/generator.py
+++ gan/generator.py
@@ -211,15 +211,7 @@ class HawkesGenerator(object):
 		params['train_count'] = len(patent)
 		params['beta'] = beta.tolist()[0]
 		params['patent'] = patent
-		params['feature_name'] = ["assignee_type",
-								"n_inventor",
-								"n_claim",
-								"n_backward",
-								"ratio_cite",
-								"generality",
-								"originality",
-								"forward_lag",
-								"backward_lag"]
+		params['feature_name'] = []
 
 		self.params = params
 
@@ -354,7 +346,7 @@ class HawkesGenerator(object):
 			pred.append(ct)
 		return pred
 
-	def create_trainable_model(self,sequences, pred_length):
+	def create_trainable_model(self,sequences, pred_length, weights={}, infinite_dimension=False):
 		from keras import backend as K
 		from keras.engine.topology import Layer
 		from keras.initializers import Constant
@@ -386,6 +378,11 @@ class HawkesGenerator(object):
 				self.pred_length = pred_length
 				self.delta = delta
 
+				self.spont_initializer = Constant(np.ones([self.nb_sequence,self.nb_type]))
+				self.Theta_initializer = Constant(np.ones([self.nb_sequence,self.nb_type]))
+				self.W_initializer = Constant(np.array([[0.5 for j in range(self.nb_type)] for i in range(self.nb_sequence)]))
+				self.Alpha_initializer = Constant(np.array([[0.5 for j in range(self.nb_type)] for i in range(self.nb_sequence)]))
+
 				super(HawkesLayer, self).__init__(**kwargs)
 
 			def build(self, input_shape):
@@ -398,19 +395,19 @@ class HawkesGenerator(object):
 											trainable=False)
 
 				self.spontaneous = self.add_weight(shape=(self.nb_sequence, self.nb_type),
-											initializer='glorot_uniform',
+											initializer=self.spont_initializer,
 											trainable=True)
 
 				self.Theta = self.add_weight(shape=(self.nb_sequence, self.nb_type),
-											initializer='glorot_uniform',
+											initializer=self.Theta_initializer,
 											trainable=True)
 
 				self.W = self.add_weight(shape=(self.nb_sequence, self.nb_type),
-											initializer='glorot_uniform',
+											initializer=self.W_initializer,
 											trainable=True)
 
 				self.Alpha = self.add_weight(shape=(self.nb_sequence, self.nb_type, self.nb_type),
-											initializer='glorot_uniform',
+											initializer=self.Alpha_initializer,
 											trainable=True)
 
 				super(HawkesLayer, self).build(input_shape)
@@ -440,7 +437,7 @@ class HawkesGenerator(object):
 				# }
 
 				pred_seq = tensor_array_ops.TensorArray(dtype=tf.float32, size=self.nb_event + self.pred_length, 
-					dynamic_size=False, infer_shape=True)
+					dynamic_size=False, infer_shape=True, clear_after_read=False)
 
 				def copy_unit(t, pred_seq):
 					pred_seq = pred_seq.write(t, self.train_seq[t])
@@ -489,6 +486,79 @@ class HawkesGenerator(object):
 				# print '@1 ',input_shape
 				return (input_shape[0], self.nb_event + self.pred_length, self.nb_type, self.nb_feature)
 
+		class InfiniteDimensionHawkesLayer(HawkesLayer):
+			def __init__(self, sequences_value, pred_length, delta = 1., **kwargs):
+
+				super(InfiniteDimensionHawkesLayer, self).__init__(sequences_value, pred_length, delta = 1., **kwargs)
+
+			def call(self, seq_id):
+				if K.dtype(seq_id) != 'int32':
+					seq_id = K.cast(seq_id, 'int32')
+
+				# seq_id = K.gather(seq_id, 0)
+				# seq_id = seq_id[0,0]
+				seq_id = K.gather(K.gather(seq_id,0),0)
+				self.train_seq = K.gather(self.sequences, seq_id)[:,:,0] # currently only support the 1st feature
+				spont  = K.gather(self.spontaneous, seq_id)
+				theta = K.gather(self.Theta, seq_id)
+				w = K.gather(self.W, seq_id)
+				alpha = K.gather(self.Alpha, seq_id)
+				# print {
+				# 	'spont':spont.shape,
+				# 	'theta':theta.shape,
+				# 	'train_seq':self.train_seq.shape,
+				# 	'alpha':alpha,
+				# 	'w':w.shape,
+				# 	'Theta':self.Theta.get_shape(),
+				# 	'Alpha':self.Alpha.get_shape(),
+				# 	'sequences':self.sequences.get_shape,
+				# 	'seq_id':seq_id.get_shape(),
+				# }
+
+				pred_seq = tensor_array_ops.TensorArray(dtype=tf.float32, size=self.nb_event + self.pred_length, 
+					dynamic_size=False, infer_shape=True, clear_after_read=False)
+
+				def copy_unit(t, pred_seq):
+					pred_seq = pred_seq.write(t, self.train_seq[t])
+					return t+1, pred_seq
+
+				def triggering_unit(int_tao, pred_seq, spont, theta, w, alpha, t, effect):
+					tao = K.cast(int_tao, 'float32')
+					effect_unit = pred_seq.read(int_tao) * (tf.exp(- w * (t - tao) * self.delta) - tf.exp(- w * (t + 1 - tao) * self.delta))
+					# print {
+					# 	"effect_unit":effect_unit.get_shape(),
+					# 	"pred_seq":pred_seq.read(int_tao).get_shape(),
+					# 	"0":(tf.exp(- w * (t - tao) * self.delta) - tf.exp(- w * (t + 1 - tao) * self.delta)).get_shape(),
+					# }
+					return int_tao + 1, pred_seq, spont, theta, w, alpha, t, effect + effect_unit
+
+				def prediction_unit(int_t, pred_seq, spont, theta, w, alpha):
+					t = K.cast(int_t, 'float32')
+					term1 = spont / theta * (tf.exp(- theta * t * self.delta) - tf.exp(- theta * (t + 1) * self.delta))
+					# term2 = tf.stack([pred_seq.read(tao) * (tf.exp(- w * (t - tao) * self.delta) - tf.exp(- w * (t + 1 - tao) * self.delta)) \
+					# 			for tao in range(int_t) ])
+					# term2 = tf.reduce_sum(term2,0)
+					_0, _1, _2, _3, _4, _5, _6, effect = control_flow_ops.while_loop(
+						cond=lambda int_tao, _1, _2, _3, _4, _5, _6, _7: int_tao < int_t,
+						body=triggering_unit,
+						loop_vars=(tf.constant(0, dtype=tf.int32),pred_seq, spont, theta, w, alpha, t, tf.constant([0.,0.],dtype=tf.float32)))
+
+					term2 = tf.reduce_sum(tf.matmul(alpha,tf.expand_dims(effect,1)),1) / w
+					pred_seq = pred_seq.write(int_t, term1 + term2)
+					return int_t+1, pred_seq, spont, theta, w, alpha
+
+				_0, pred_seq = control_flow_ops.while_loop(
+					cond=lambda t, pred_seq: t < self.nb_event,
+					body=copy_unit,
+					loop_vars=(tf.constant(0, dtype=tf.int32),pred_seq))
+
+				_0, pred_seq, _2, _3, _4, _5 = control_flow_ops.while_loop(
+					cond=lambda int_t, _1, _2, _3, _4, _5: int_t < self.nb_event + self.pred_length,
+					body=prediction_unit,
+					loop_vars=(tf.constant(self.nb_event, dtype=tf.int32),pred_seq,spont,theta,w,alpha))
+
+				pred_seq = tf.expand_dims(tf.expand_dims(pred_seq.stack(), 2), 0)  # currently only support the 1st feature and one sample per batch
+				return pred_seq
 
 		layer = HawkesLayer(sequences,pred_length)
 		x = Input(shape=(1,), dtype='int32')
